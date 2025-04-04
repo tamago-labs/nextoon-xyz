e803c4cc6d26732644c7d6f7cfcb8db9c5524997
pisuthd <pisuth.dae@gmail.com>
2025-04-04 15:21:49 +0900

    Add content schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,10 +7,26 @@ const schema = a.schema({
       username: a.string().required(), // wallet address
       email: a.string(),
       displayName: a.string(),
-      isApprovedCreator: a.boolean()
+      contents: a.hasMany('Content', "userId"),
     })
     .authorization((allow) => [
       allow.publicApiKey()
+    ]),
+  Content: a
+    .model({
+      userId: a.id().required(),
+      user: a.belongsTo('User', "userId"),
+      title: a.string(),
+      description: a.string(),
+      coverImageUrl: a.string(),
+      isTestnet: a.boolean(),
+      tokenName: a.string(),
+      tokenSymbol: a.string(),
+      tokenContract: a.string()
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner()
     ]),
 });
 
